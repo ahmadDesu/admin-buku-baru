1cd71bfe3fb42ce72f789ceee398bda5c3f3fa54
Ahmad Adyla <madrista982@gmail.com>
2022-08-02 11:07:13 +0700

    Remove values when submit button on click

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -24,89 +24,167 @@
             <!-- Judul -->
             <div class="mb-3">
                 <label for="judul" class="form-label">Judul</label>
-                <input type="text" class="form-control" id="judul" name="judul" placeholder="Masukan Judul" required autofocus>
+                <textarea type="text" class="form-control" id="form-konten" name="judul" placeholder="Masukan Judul" required autofocus></textarea>
             </div>
 
             <!-- Link time -->
             <div class="mb-3">
                 <label for="time" class="form-label">Tanggal</label>
-                <input type="text" class="form-control" id="time" name="time" placeholder="Masukan Tanggal" required autofocus>
+                <textarea type="text" class="form-control" id="form-konten" name="time" placeholder="Masukan Tanggal" required autofocus></textarea>
             </div>
 
             <!-- Youtube -->
             <div class="mb-3">
                 <label for="youtube" class="form-label">Youtube</label>
-                <input type="text" class="form-control" id="youtube" name="youtube" placeholder="Link Youtube">
+                <textarea type="text" class="form-control" id="form-konten" name="youtube" placeholder="Link Youtube"></textarea>
             </div>
 
-            <!-- Konten 1 -->
+            <!-- artikel 1 -->
             <div class="mb-3">
-                <label for="konten1" class="form-label">Artikel 1</label>
-                <input type="text" class="form-control" id="konten1" name="konten1" placeholder="Artikel 1">
-            </div>
-            <!-- Kode 1  -->
-            <label for="kode-1" class="form-label">Kode 1</label>
-            <br>
-            <textarea id="kode1" name="kode1" rows="5" cols="148" placeholder="Kode 1"></textarea>
-            <!-- Foto konten 1 -->
-            <div class="mb-3">
-                <label for="foto_konten3" class="form-label">Foto konten 1</label>
-                <input type="text" class="form-control" id="form" name="foto_konten1" placeholder="Link foto konten 1">
+                <label for="artikel1" class="form-label">Artikel 1</label>
+                <textarea type="text" class="form-control" id="form-konten" name="artikel1" placeholder="Artikel 1"></textarea>
             </div>
 
-            <!-- Konten 2 -->
+            <!-- artikel 2 -->
             <div class="mb-3">
                 <label for="artikel2" class="form-label">Artikel 2</label>
-                <input type="text" class="form-control" id="form" name="konten2" placeholder="Artikel 2">
+                <textarea type="text" class="form-control" id="form-konten" name="artikel2" placeholder="Artikel 2"></textarea>
             </div>
             <!-- Kode 2 -->
             <label for="kode-2" class="form-label">Kode 2</label>
             <br>
-            <textarea id="kode2" name="kode2" rows="5" cols="148" placeholder="Kode 2"></textarea>
-            <!-- Foto konten 2 -->
+            <textarea id="form-konten" name="kode2" rows="5" cols="148" class="form-control" placeholder="Kode 2"></textarea>
+            <!-- Foto artikel 2 -->
             <div class="mb-3">
-                <label for="foto_konten2" class="form-label">Foto konten 2</label>
-                <input type="text" class="form-control" id="form" name="foto_konten2" placeholder="Link foto konten 2">
+                <label for="foto_artikel2" class="form-label">Foto artikel 2</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel2" placeholder="Link foto artikel 2"></textarea>
             </div>
 
-            <!-- Konten 3 -->
+            <!-- artikel 3 -->
             <div class="mb-3">
                 <label for="artikel3" class="form-label">Artikel 3</label>
-                <input type="text" class="form-control" id="form" name="konten3" placeholder="Artikel 3">
+                <textarea type="text" class="form-control" id="form-konten" name="artikel3" placeholder="Artikel 3"></textarea>
             </div>
             <!-- Kode 3  -->
             <label for="kode-3" class="form-label">Kode 3</label>
             <br>
-            <textarea id="kode3" name="kode3" rows="5" cols="148" placeholder="Kode 3"></textarea>
-            <!-- Foto konten 3 -->
+            <textarea id="form-konten" name="kode3" rows="5" cols="148" class="form-control" placeholder="Kode 3"></textarea>
+            <!-- Foto artikel 3 -->
             <div class="mb-3">
-                <label for="foto_konten3" class="form-label">Foto konten 3</label>
-                <input type="text" class="form-control" id="form" name="foto_konten3" placeholder="Link foto konten 3">
+                <label for="foto_artikel3" class="form-label">Foto artikel 3</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel3" placeholder="Link foto artikel 3"></textarea>
             </div>
 
-            <!-- Konten 4 -->
+            <!-- artikel 4 -->
             <div class="mb-3">
                 <label for="artikel4" class="form-label">Artikel 4</label>
-                <input type="text" class="form-control" id="form" name="konten4" placeholder="Artikel 4">
+                <textarea type="text" class="form-control" id="form-konten" name="artikel4" placeholder="Artikel 4"></textarea>
             </div>
-            <!-- Foto konten 4 -->
+            <!-- Kode 4 -->
             <div class="mb-3">
-                <label for="img" class="form-label">Foto konten 4</label>
-                <input type="text" class="form-control" id="form" name="img" placeholder="Link foto konten 4">
+                <label for="kode4" class="form-label">Kode 4</label>
+                <textarea type="text" class="form-control" id="form-konten" name="kode4" placeholder="Kode 4"></textarea>
+            </div>
+            <!-- Foto artikel 4 -->
+            <div class="mb-3">
+                <label for="foto_artikel4" class="form-label">Foto artikel 4</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel4" placeholder="Link foto artikel 4"></textarea>
             </div>
 
-            <!-- Konten 5  -->
+            <!-- artikel 5  -->
             <div class="mb-3">
                 <label for="artikel5" class="form-label">Artikel 5</label>
-                <input type="text" class="form-control" id="form" name="konten5" placeholder="Artikel 5">
+                <textarea type="text" class="form-control" id="form-konten" name="artikel5" placeholder="Artikel 5"></textarea>
             </div>
+            <!-- Kode 5 -->
+            <label for="kode-5" class="form-label">Kode 5</label>
+            <br>
+            <textarea id="kode5" name="kode5" rows="5" cols="148" class="form-control" placeholder="Kode 5"></textarea>
+            <!-- Foto artikel 5 -->
+            <div class="mb-3">
+                <label for="foto_artikel5" class="form-label">Foto artikel 5</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel5" placeholder="Link foto artikel 5"></textarea>
+            </div>
+
+            <!-- artikel 6 -->
+            <div class="mb-3">
+                <label for="artikel6" class="form-label">Artikel 6</label>
+                <textarea type="text" class="form-control" id="form-konten" name="artikel6" placeholder="Artikel 6"></textarea>
+            </div>
+            <!-- Kode 6 -->
+            <label for="kode-6" class="form-label">Kode 6</label>
+            <br>
+            <textarea id="kode6" name="kode6" rows="5" cols="148" class="form-control" placeholder="Kode 6"></textarea>
+            <!-- Foto artikel 6 -->
+            <div class="mb-3">
+                <label for="foto_artikel6" class="form-label">Foto artikel 6</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel6" placeholder="Link foto artikel 6"></textarea>
+            </div>
+
+            <!-- artikel 7 -->
+            <div class="mb-3">
+                <label for="artikel7" class="form-label">Artikel 7</label>
+                <textarea type="text" class="form-control" id="form-konten" name="artikel7" placeholder="Artikel 7"></textarea>
+            </div>
+            <!-- Kode 7 -->
+            <label for="kode-7" class="form-label">Kode 7</label>
+            <br>
+            <textarea id="kode7" name="kode7" rows="5" cols="148" class="form-control" placeholder="Kode 7"></textarea>
+            <!-- Foto artikel 7 -->
+            <div class="mb-3">
+                <label for="foto_artikel7" class="form-label">Foto artikel 7</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel7" placeholder="Link foto artikel 7"></textarea>
+            </div>
+
+            <!-- artikel 8 -->
+            <div class="mb-3">
+                <label for="artikel8" class="form-label">Artikel 8</label>
+                <textarea type="text" class="form-control" id="form-konten" name="artikel8" placeholder="Artikel 8"></textarea>
+            </div>
+            <!-- Kode 8 -->
+            <label for="kode-8" class="form-label">Kode 8</label>
+            <br>
+            <textarea id="kode8" name="kode8" rows="5" cols="148" class="form-control" placeholder="Kode 8"></textarea>
+            <!-- Foto artikel 8 -->
+            <div class="mb-3">
+                <label for="foto_artikel8" class="form-label">Foto artikel 8</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel8" placeholder="Link foto artikel 8"></textarea>
+            </div>
+
+            <!-- artikel 9 -->
+            <div class="mb-3">
+                <label for="artikel9" class="form-label">Artikel 9</label>
+                <textarea type="text" class="form-control" id="form-konten" name="artikel9" placeholder="Artikel 9"></textarea>
+            </div>
+            <!-- Kode 9 -->
+            <label for="kode-9" class="form-label">Kode 9</label>
+            <br>
+            <textarea id="kode9" name="kode9" rows="5" cols="148" class="form-control" placeholder="Kode 9"></textarea>
+            <!-- Foto artikel 9 -->
+            <div class="mb-3">
+                <label for="foto_artikel9" class="form-label">Foto artikel 9</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel9" placeholder="Link foto artikel 9"></textarea>
+            </div>
+
+            <!-- artikel 10 -->
+            <div class="mb-3">
+                <label for="artikel10" class="form-label">Artikel 10</label>
+                <textarea type="text" class="form-control" id="form-konten" name="artikel10" placeholder="Artikel 10"></textarea>
+            </div>
+            <!-- Kode 10 -->
+            <label for="kode-10" class="form-label">Kode 10</label>
+            <br>
+            <textarea id="kode10" name="kode10" rows="5" cols="148" class="form-control" placeholder="Kode 10"></textarea>
+            <!-- Foto artikel 10 -->
+            <div class="mb-3">
+                <label for="foto_artikel10" class="form-label">Foto artikel 10</label>
+                <textarea type="text" class="form-control" id="form-konten" name="foto_artikel10" placeholder="Link foto artikel 10"></textarea>
+            </div>
+
 
             <input id="uid" name="uid" type="hidden" value="{{ \Session::get('uid') }}" class="form-control ">
             <button id="submitArtikel" type="button" class="btn btn-primary mb-2">Submit</button>
         </form>
-
-        <button>Halo</button>
-
 
         {{--Firebase Tasks--}}
         <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
@@ -132,18 +210,34 @@
         		<td>' + value.judul + '</td>\
                 <td>' + value.time + '</td>\
                 <td>' + value.youtube + '</td>\
-        		<td>' + value.konten1 + '</td>\
-                <td>' + value.konten2 + '</td>\
-                <td>' + value.konten3 + '</td>\
-                <td>' + value.konten4 + '</td>\
-                <td>' + value.konten5 + '</td>\
-                <td>' + value.kode1 + '</td>\
+        		<td>' + value.artikel1 + '</td>\
+                <td>' + value.artikel2 + '</td>\
+                <td>' + value.artikel3 + '</td>\
+                <td>' + value.artikel4 + '</td>\
+                <td>' + value.artikel5 + '</td>\
+                <td>' + value.artikel6 + '</td>\
+                <td>' + value.artikel7 + '</td>\
+                <td>' + value.artikel8 + '</td>\
+                <td>' + value.artikel9 + '</td>\
+                <td>' + value.artikel10 + '</td>\
                 <td>' + value.kode2 + '</td>\
                 <td>' + value.kode3 + '</td>\
-                <td>' + value.foto_konten1 + '</td>\
-                <td>' + value.foto_konten2 + '</td>\
-                <td>' + value.foto_konten3 + '</td>\
-                <td>' + value.img + '</td>\
+                <td>' + value.kode4 + '</td>\
+                <td>' + value.kode5 + '</td>\
+                <td>' + value.kode6 + '</td>\
+                <td>' + value.kode7 + '</td>\
+                <td>' + value.kode8 + '</td>\
+                <td>' + value.kode9 + '</td>\
+                <td>' + value.kode10 + '</td>\
+                <td>' + value.foto_artikel2 + '</td>\
+                <td>' + value.foto_artikel3 + '</td>\
+                <td>' + value.foto_artikel4 + '</td>\
+                <td>' + value.foto_artikel5 + '</td>\
+                <td>' + value.foto_artikel6 + '</td>\
+                <td>' + value.foto_artikel7 + '</td>\
+                <td>' + value.foto_artikel8 + '</td>\
+                <td>' + value.foto_artikel9 + '</td>\
+                <td>' + value.foto_artikel10 + '</td>\
         		<td><button data-toggle="modal" data-target="#update-modal" class="btn btn-info updateData" data-id="' + index + '">Update</button>\
         		<button data-toggle="modal" data-target="#remove-modal" class="btn btn-danger removeData" data-id="' + index + '">Delete</button></td>\
         	</tr>');
@@ -160,22 +254,43 @@
                 var time = values[1].value;
                 var youtube = values[2].value;
 
-                var konten1 = values[3].value;
-                var kode1 = values[4].value;
-                var foto_konten1 = values[5].value;
+                var artikel1 = values[3].value;
 
-                var konten2 = values[6].value;
-                var kode2 = values[7].value;
-                var foto_konten2 = values[8].value;
+                var artikel2 = values[4].value;
+                var kode2 = values[5].value;
+                var foto_artikel2 = values[6].value;
 
-                var konten3 = values[9].value;
-                var kode3 = values[10].value;
-                var foto_konten3 = values[11].value;
+                var artikel3 = values[7].value;
+                var kode3 = values[8].value;
+                var foto_artikel3 = values[9].value;
 
-                var konten4 = values[12].value;
-                var img = values[13].value;
+                var artikel4 = values[10].value;
+                var kode4 = values[11].value;
+                var foto_artikel4 = values[12].value;
 
-                var konten5 = values[14].value;
+                var artikel5 = values[13].value;
+                var kode5 = values[14].value;
+                var foto_artikel5 = values[15].value;
+
+                var artikel6 = values[16].value;
+                var kode6 = values[17].value;
+                var foto_artikel6 = values[18].value;
+
+                var artikel7 = values[19].value;
+                var kode7 = values[20].value;
+                var foto_artikel7 = values[21].value;
+
+                var artikel8 = values[22].value;
+                var kode8 = values[23].value;
+                var foto_artikel8 = values[24].value;
+
+                var artikel9 = values[25].value;
+                var kode9 = values[26].value;
+                var foto_artikel9 = values[27].value;
+
+                var artikel10 = values[28].value;
+                var kode10 = values[29].value;
+                var foto_artikel10 = values[30].value;
 
 
                 var userID = lastIndex + 1;
@@ -186,29 +301,50 @@
                     time: time,
                     youtube: youtube,
 
-                    konten1: konten1,
-                    konten2: konten2,
-                    konten3: konten3,
-                    konten4: konten4,
-                    konten5: konten5,
+                    artikel1: artikel1,
+                    artikel2: artikel2,
+                    artikel3: artikel3,
+                    artikel4: artikel4,
+                    artikel5: artikel5,
+                    artikel6: artikel6,
+                    artikel7: artikel7,
+                    artikel8: artikel8,
+                    artikel9: artikel9,
+                    artikel10: artikel10,
 
-                    kode1: kode1,
                     kode2: kode2,
                     kode3: kode3,
+                    kode4: kode4,
+                    kode5: kode5,
+                    kode6: kode6,
+                    kode7: kode7,
+                    kode8: kode8,
+                    kode9: kode9,
+                    kode10: kode10,
 
-                    img: img,
-                    foto_konten1: foto_konten1,
-                    foto_konten2: foto_konten2,
-                    foto_konten3: foto_konten3,
+                    foto_artikel2: foto_artikel2,
+                    foto_artikel3: foto_artikel3,
+                    foto_artikel4: foto_artikel4,
+                    foto_artikel6: foto_artikel6,
+                    foto_artikel7: foto_artikel7,
+                    foto_artikel8: foto_artikel8,
+                    foto_artikel9: foto_artikel9,
+                    foto_artikel10: foto_artikel10,
                 });
                 // Reassign lastID value
                 lastIndex = userID;
                 $("#addCustomer input").val("");
+
+                // remove values on textarea
+               
+                let change = document.getElementsByClassName("form-control");
+                for (let i = 0; i < change.length; i++) {
+                    change[i].value = "";
+                }
             });
         </script>
 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
 </body>
 
 </html>
